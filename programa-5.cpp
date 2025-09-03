d3e3761776e1c1b54632b0697ccbc71f0aed100b
JoelHanerth <hanerthjoel1@gmail.com>
2025-09-03 14:21:42 -0300

    Adiciona funções de busca e exclusão de alunos na tabela hash, além de implementar um menu interativo para gerenciamento de alunos. Inclui também a estrutura de dados para controle de remoção e otimiza a função de impressão.

--- programa-5.cpp
+++ programa-5.cpp
@@ -1,34 +1,11 @@
-/*
-Crie mais 3 hashs de tamanhos distintos para o cpf, utilize alguma ideia sua para cada uma. (no lugar de usar os dois ultimos digitos do cpf). Faça uma com 1000 índices no vetor, 10000 índices e 100000 índices. Faça a analise do tempo de inserção de todos os elementos e veja o que ocorre.
-Analise o impacto do tempo da busca por nome. Crie uma busca automática que busque por 10 nomes, e rode o tempo disso nos 4 hashs implementados.
-Após isso, volte para a hash de 100, e mude a forma de ordenação na lista. Passe a ordenar por cpf, e após inserir tudo, rode um algoritmo de ordenação, de preferência o quicksort em cada lista da hash. Faça a comparação de tempo para inserir tudo entre essa forma e a forma inicial que é ordenando por nome.
-
-Abaixo segue o tempo gasto com a adição feita pelo nome:
-Tempo gasto com 100 hash: 
-Tempo para carregar arquivo: 706.161 segundos
-Tempo para buscar 10 alunos: 0.645 segundos
-
-Tempo gasto com 1000 hash:
-Tempo para carregar arquivo: 67.303 segundos
-Tempo para buscar 10 alunos: 0.528 segundos
-
-Tempo gasto com 10000 hash:
-Tempo para carregar arquivo: 10.312 segundos
-Tempo para buscar 10 alunos: 0.521 segundos
-
-Tempo gasto com 100000 hash:
-Tempo para carregar arquivo: 4.413 segundos
-Tempo para buscar 10 alunos: 0.478 segundos
-*/
-
-
 #include <fstream>
 #include <sstream>
 #include <ctime>
 #include <iostream>
 #include <cmath>
+#include <locale>
 
-#define TAMANHO_HASH_INICIAL 7
+#define TAMANHO_HASH_INICIAL 11
 
 using namespace std;
 
@@ -47,6 +24,7 @@ struct Aluno{
 struct hashTable {
     Aluno **hash;
     bool *hashOcupada;
+    bool *hashRemovida;
     int tamanhoAtualHash;
     int quantidade;
 };
@@ -56,23 +34,37 @@ void inicializaHashTable(hashTable *tabela, int tamanho) {
     tabela->quantidade = 0;
     tabela->hash = new Aluno*[tabela->tamanhoAtualHash];
     tabela->hashOcupada = new bool[tabela->tamanhoAtualHash];
+    tabela->hashRemovida = new bool[tabela->tamanhoAtualHash];
     for (int i = 0; i < tabela->tamanhoAtualHash; i++) {
         tabela->hashOcupada[i] = false;
+        tabela->hashRemovida[i] = false;
     }
 }
 
-int calculaHash(string nome, int tamanho) {
-    long sum = 0;
+// int calculaHash(string nome, int tamanho) {
+//     long sum = 0;
     
+//     for (char c : nome) {
+//         sum += c;
+//     }
+    
+//     long cubo = sum * sum;
+    
+//     int indice = cubo % tamanho;
+//     if (indice < 0) indice += tamanho;
+//     return indice;
+// }
+
+int calculaHash(string nome, int tamanho) {
+    unsigned int hash = 0;
+    
+    // FNV-1a hash algorithm
     for (char c : nome) {
-        sum += c;
+        hash ^= c;
+        hash *= 16777619; // FNV prime
     }
     
-    long cubo = sum * sum * sum;
-    
-    int indice = cubo % tamanho;
-    if (indice < 0) indice += tamanho;
-    return indice;
+    return hash % tamanho;
 }
 
 // Function to return nearest prime number
@@ -114,14 +106,17 @@ void reHash(hashTable *tabela){
             }
             novaTabela.hash[indice] = aluno;
             novaTabela.hashOcupada[indice] = true;
+            novaTabela.hashRemovida[indice] = false;
         }
     }
 
     delete[] tabela->hash;
     delete[] tabela->hashOcupada;
+    delete[] tabela->hashRemovida;
 
     tabela->hash = novaTabela.hash;
     tabela->hashOcupada = novaTabela.hashOcupada;
+    tabela->hashRemovida = novaTabela.hashRemovida;
     tabela->tamanhoAtualHash = novoTamanho;
 }
 
@@ -133,15 +128,19 @@ void inserir(hashTable *tabela, Aluno *novo_aluno) {
 
     int indice = calculaHash(novo_aluno->nome, tabela->tamanhoAtualHash);
     while (tabela->hashOcupada[indice]) {
+        if (tabela->hash[indice]->nome == novo_aluno->nome) {
+            tabela->hash[indice] = novo_aluno;
+            return;
+        }
         indice = (indice + 1) % tabela->tamanhoAtualHash;
     }
     tabela->hash[indice] = novo_aluno;
     tabela->hashOcupada[indice] = true;
+    tabela->hashRemovida[indice] = false;
     tabela->quantidade++;
 }
 
 void carregarArquivo(hashTable *tabela, string diretorio) {
-    printf("aqui\n");
     ifstream arquivo(diretorio);
     string linha;
     if (!arquivo.is_open()) {
@@ -176,28 +175,132 @@ void carregarArquivo(hashTable *tabela, string diretorio) {
     arquivo.close();
 }
 
+void imprimirAluno(Aluno *aluno){
+    cout << "Aluno encontrado:\n";
+    cout << "Matrícula: " << aluno->matricula << endl;
+    cout << "CPF: " << aluno->cpf << endl;
+    cout << "Nome: " << aluno->nome << endl;
+    cout << "Nota: " << aluno->nota << endl;
+    cout << "Idade: " << aluno->idade << endl;
+    cout << "Curso: " << aluno->curso << endl;
+    cout << "Cidade: " << aluno->cidade << endl;
+}
 void imprimir(hashTable *tabela){
+    cout << "-------------------------" << endl;
     for (int i = 0; i < tabela->tamanhoAtualHash; i++) {
         if (tabela->hashOcupada[i]) {
-            cout << "Matrícula: " << tabela->hash[i]->matricula << endl;
-            cout << "CPF: " << tabela->hash[i]->cpf << endl;
-            cout << "Nome: " << tabela->hash[i]->nome << endl;
-            cout << "Nota: " << tabela->hash[i]->nota << endl;
-            cout << "Idade: " << tabela->hash[i]->idade << endl;
-            cout << "Curso: " << tabela->hash[i]->curso << endl;
-            cout << "Cidade: " << tabela->hash[i]->cidade << endl;
+            imprimirAluno(tabela->hash[i]);
             cout << "-------------------------" << endl;
+        }
+    }
+}
+bool buscarNome(hashTable *tabela, string nome){
+    int indice = calculaHash(nome, tabela->tamanhoAtualHash);
+    int indice_inicial = indice;
+    
+    while (tabela->hashOcupada[indice] || tabela->hashRemovida[indice]) {
+        if (tabela->hashOcupada[indice] && tabela->hash[indice]->nome == nome) {
+            imprimirAluno(tabela->hash[indice]);
+            return true;
+        }
+        indice = (indice + 1) % tabela->tamanhoAtualHash;
+        if (indice == indice_inicial) break;
+    }
+    
+    cout << "Aluno com nome " << nome << " não encontrado." << endl;
+    return false;
+}
+
+
+bool excluir(hashTable *tabela, string nome) {
+    int indice = calculaHash(nome, tabela->tamanhoAtualHash);
+    int indice_inicial = indice;
+    
+    while (tabela->hashOcupada[indice] || tabela->hashRemovida[indice]) {
+        if (tabela->hashOcupada[indice] && tabela->hash[indice]->nome == nome) {
+            delete tabela->hash[indice];
+            tabela->hashOcupada[indice] = false;
+            tabela->hashRemovida[indice] = true;
+            tabela->quantidade--;
+            return true;
+        }
+        indice = (indice + 1) % tabela->tamanhoAtualHash;
+        if (indice == indice_inicial) break;
+    }
+    return false;
+}
+
+void confirmarExclusao(hashTable *tabela, string nome){
+    string opcao;
+    if (buscarNome(tabela, nome)){
+        cout << "Deseja excluír esse aluno? [S/N]";
+        cin >> opcao;
+        if (opcao == "s" || opcao == "S") {
+            if (excluir(tabela, nome)) {
+                printf("Aluno com nome %s excluido com sucesso.\n", nome.c_str());
+            } else {
+                printf("Erro ao excluir aluno com nome %s.\n", nome.c_str());
+            }
+        } else {
+            printf("Aluno com nome %s nao excluido.\n", nome.c_str());
         }
     }
 }
 
 
 
+void menu(hashTable *tabela){
+    int opcao;
+    string matricula;
+    string nome;
+
+    do {
+        printf("\nMenu:\n");
+        printf("1. Imprimir Alunos\n");
+        printf("2. Buscar Aluno\n");
+        printf("3. Excluir Aluno\n");
+        printf("4. Sair\n");
+        printf("Escolha uma opcao: ");
+        cin >> opcao;
+
+        switch (opcao) {
+            case 1:
+                imprimir(tabela);
+                break;
+            case 2:
+                cout << "Digite o nome do aluno a ser buscado: ";
+                cin.ignore();
+                getline(cin, nome);
+                buscarNome(tabela, nome);
+                break;
+            case 3:
+                cout << "Digite o nome do aluno a ser excluído: ";
+                cin.ignore();
+                getline(cin, nome);
+                confirmarExclusao(tabela, nome);
+                break;
+            case 4:
+                printf("Saindo...\n");
+                break;
+            default:
+                printf("Opcao invalida!\n");
+                break;
+        }
+    } while (opcao != 4);
+}
+
 hashTable tabela;
 
 int main(){
+    setlocale(LC_ALL, "pt_BR.UTF-8");
+    clock_t inicio = clock();
     inicializaHashTable(&tabela, TAMANHO_HASH_INICIAL);
     carregarArquivo(&tabela,"../alunos_completosV2.csv");
     // imprimir(&tabela);
+    clock_t fim = clock();
+    double tempo = double(fim - inicio) / CLOCKS_PER_SEC;
+    cout << "Tempo de execução: " << tempo << " segundos" << endl;
+    cout << "Quantidade de alunos: " << tabela.quantidade << endl;
+    menu(&tabela);
     return 0;
 }
